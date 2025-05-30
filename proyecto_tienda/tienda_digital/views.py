554a374894c985816d3d32d3975b4119db7e132f
Nicklix121 <andrediaz1234512345@gmail.com>
2025-05-30 16:39:17 -0400

    se agregan imagenes

--- proyecto_tienda/tienda_digital/views.py
+++ proyecto_tienda/tienda_digital/views.py
@@ -2,16 +2,25 @@ from django.shortcuts import redirect, render
 from tienda_digital.models import Producto
 from tienda_digital.forms import ProductoForm
 from django.forms import modelform_factory
+from django.core.paginator import Paginator
 
 
 ProductoForm = modelform_factory(Producto, exclude=[])
 
+
+
 def mostrar_productos(request):
-
     productos = Producto.objects.all()
-    info_prod = {'productos': productos}
 
-    return render(request, 'mostrar_productos.html', info_prod )
+    items_por_pagina = request.GET.get('paginador', 12)
+
+    page_number = request.GET.get('page')
+
+    paginador = Paginator(productos, items_por_pagina)
+    page_obj = paginador.get_page(page_number)
+
+    return render(request, 'mostrar_productos.html', {'page_obj': page_obj,})
+
 
 
 def agregar_producto(request):
